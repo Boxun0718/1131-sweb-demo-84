2138b3b3417a19b8bf9bed0cb0d8390278f748f0
Boxun0718 <213410284@o365.tku.edu.tw>
2024-10-26 11:10:14 +0800

    add index.html as home page

--- demo/md/w07_84.js
+++ demo/md/w07_84.js
@@ -1,1 +1,16 @@
-alert('hello')
+const p = document.querySelector('.show-classdemo')
+console.log('show-classdemo', p)
+
+function showDemo(item) {
+  switch (item) {
+    case 'w02_intro_84':
+      p.innerHTML = `<iframe src='./w02_intro_84' width="100%" height="100%" />`
+      break
+    case 'w03_card_84':
+      p.innerHTML = `<iframe src='./w03_card_84' width="100%" height="100%" />`
+      break
+    case 'w06_blog_84':
+      p.innerHTML = `<iframe src='./w06_blog_84' width="100%" height="100%" />`
+      break
+  }
+}
