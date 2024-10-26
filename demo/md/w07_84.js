7bc6113ec5a50131c829b5d18ee002a899a016ed
Boxun0718 <213410284@o365.tku.edu.tw>
2024-10-26 13:27:29 +0800

    w07-p4: Create navbar with links to class demo w02, w03, w06, and deploy it to Vercel

--- demo/md/w07_84.js
+++ demo/md/w07_84.js
@@ -7,7 +7,7 @@ function showDemo(item) {
       p.innerHTML = `<iframe src='./w02-intro-84' width="100%" height="100%" />`
       break
     case 'w03_card_84':
-      p.innerHTML = `<iframe src='./w03' width="100%" height="100%" />`
+      p.innerHTML = `<iframe src='./w03_card_84' width="100%" height="100%" />`
       break
     case 'w06_blog_84':
       p.innerHTML = `<iframe src='./w06_blog_84' width="100%" height="100%" />`
